bcbf375d1fc191c07a436201c0e8f19352d8e4ea
01 <hemmatzahraaa@gmail.com>
2022-05-01 15:55:35 +0430

    test

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+import cv2
+import matplotlib.pyplot as plt
+
+
+def read_images(img):
+    img = cv2.imread(img, 0)
+    plt.imshow(img, cmap='gray')
+    plt.show()
+read_images('.//images//10.jpg')
